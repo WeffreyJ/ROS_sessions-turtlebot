009bf25afdb44a472223e0615a5ceb4724551243
Jeffrey_Walker <kwekuabekujeffrey@yahoo.com>
2024-02-12 16:07:33 +0000

    circle

--- catkin_ws/src/lab1/scripts/my_turtlebot.py
+++ catkin_ws/src/lab1/scripts/my_turtlebot.py
@@ -1,93 +1,35 @@
 #!/usr/bin/env python3
 
+import rospy
+from geometry_msgs.msg import Twist
+from math import cos, sin, pi
 
+class MyTurtle:
+    def __init__(self):
+        rospy.init_node('my_turtlebot_node')
+        self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        self.angular_speed = 0.7
 
-class myTurtle():
-    
-    
-    def __init__(self) -> None:
-        """_summary_
-        
-        create all the nessary pubs/subs here and all the nessary other things
-        
-        """
-        pass 
-    
-        
+    def stop(self):
+        twist_cmd = Twist()
+        self.cmd_pub.publish(twist_cmd)
 
-    def nav_to_pose(self, goal):
-        # type: (PoseStamped) -> None
-        """
-        This is a callback function. It should extract data from goal, drive in a striaght line to reach the goal and
-        then spin to match the goal orientation.
-        :param goal: PoseStamped
-        :return:
-        """
-        pass
-
-    def odom_cb(self,msg:Odometry) ->None:
-        """_summary_
-
-        Get the odom and update the internal location of the robot
-        Args:
-            msg (Odometry): _description_
-        """
-        pass
-    
-    def stop(self)->None:
-        """_summary_
-        
-        Stop moving
-        """
-        pass
-        
-        
-    def drive_straight(self, dist: float, vel: float)->None:
-        """_summary_
-
-        Args:
-            dist (_type_): _description_
-        """
-        pass
-        
-    
-    def spin_wheels(self, u1, u2, time):
-        """
-        Spin the two wheels
-
-        :param u1: wheel 1 speed
-        :param u2: wheel 2 speed
-        :param time: time to drive
-        :return: None
-        """
-        pass
-
-    def rotate(self, angle):
-        """
-        Rotate in place
-        :param angle: angle to rotate
-        :return: None
-        """
-        pass
-    
-    def convert_to_euler(self, quat):
-        # type: (Quaternion) -> float
-        """
-        This might be helpful to have
-        :param quat: quaternion 
-        :return: euler angles
-        """
-        
+    def drive_in_circle(self, radius: float):
+        linear_speed = self.angular_speed * radius
+        twist_cmd = Twist()
+        twist_cmd.linear.x = linear_speed
+        twist_cmd.angular.z = self.angular_speed
+        self.cmd_pub.publish(twist_cmd)
 
 def main():
-    """_summary_
-    create all the node start up here
-    """
-    
-    
+    turtle = MyTurtle()
 
+    # Drive in a circle with radius 0.5m indefinitely
+    while not rospy.is_shutdown():
+        turtle.drive_in_circle(radius=0.5)
 
-
+    # Stop the turtlebot
+    turtle.stop()
 
 if __name__ == '__main__':
     main()
